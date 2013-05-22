8a84fd4be54389c191561ffd98dd972e8d6a5235
永良 慶太 <Keita@CooksoniaMBA.local>
2013-05-22 23:42:25 +0900

    a

--- public/access_check.class.php
+++ public/access_check.class.php
@@ -2,8 +2,7 @@
 class access_check{
 	
 	function check($userID, $accessToken){
-		$userIDescape = mysql_real_escape_string($userID);
-		$result = mysql_query("select accessToken from userInfo where userID = \"$userIDescape\"");
+		$result = mysql_query("select accessToken from userInfo where userID = \"$userID\"");
 		$row = mysql_fetch_assoc($result);
 		$getToken = $row['accessToken'];
 		
