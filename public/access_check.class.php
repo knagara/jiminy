e3879be543a7a6431508e617557ef9b98f55a25b
永良 慶太 <Keita@CooksoniaMBA.local>
2013-05-23 23:01:58 +0900

    is_numeric

--- public/access_check.class.php
+++ public/access_check.class.php
@@ -1,16 +1,23 @@
 <?php
-class access_check{
-	
-	function check($userID, $accessToken){
-		$result = mysql_query("select accessToken from userInfo where userID = $userID");
-		$row = mysql_fetch_assoc($result);
-		$getToken = $row['accessToken'];
-		
-		if(strcmp($accessToken, $getToken)==0){
-			return 0;	//認証成功
-		}else{
-			return 1;	//エラー
+class access_check {
+
+	function check($userID, $accessToken) {
+		if (is_numeric($userID)) {
+			$result = mysql_query("select accessToken from userInfo where userID = $userID LIMIT 1");
+			$row = mysql_fetch_assoc($result);
+			$getToken = $row['accessToken'];
+
+			if (strcmp($accessToken, $getToken) == 0) {
+				return 0;
+				//認証成功
+			} else {
+				return 1;
+				//エラー
+			}
+		} else {
+			return 1;
 		}
 	}
+
 }
 ?>
