dfd27cb56b4d3f47afd1d609fb63dbce6ca11c71
永良 慶太 <Keita@CooksoniaMBA.local>
2013-05-24 22:35:07 +0900

    suser

--- public/getUserInfo.php
+++ public/getUserInfo.php
@@ -4,47 +4,42 @@ header("Content-type: text/html; charset=UTF-8");
 <?php
 
 //データベース接続
-include("db_connect.class.php");
+include ("db_connect.class.php");
 $con = new db_connect();
-$con->jiminy_connect();
+$con -> jiminy_connect();
 
 //テーブル選択
-$table  = 'userInfo';
-
-
+$table = 'userInfo';
 
 //POST受け取り確認
-if (isset($_POST["userID"]))
-{
+if (isset($_POST["userID"])) {
 	$userID = mysql_real_escape_string($_POST["userID"]);
 	$accessToken = $_POST["accessToken"];
-	
+
 	/* userIDとaccessTokenの照合 */
-	include("access_check.class.php");
+	include ("access_check.class.php");
 	$objCheck = new access_check();
-	$accessResult = $objCheck->check($userID,$accessToken);
-	
-	if($accessResult==0)
-	{		 
-		//$categoryID = $_POST["userID"];
-		
-		/* データ をJSONで返す*/
-		include("php/mysql2json.class.php"); //JSON整形してくれるクラスファイルinclude
-		//$result = mysql_query("select i.tagName,i.tagID,count(u.tagID) from tagInfo as i,userTag as u where categoryID = \"$categoryID\"");
-		$result = mysql_query("select * from $table where userID = $userID LIMIT 1");
-		$num = mysql_affected_rows();
-		$objJSON = new mysql2json();
-		print(trim($objJSON->getJSON($result,$num)));
-	}
-	else
-	{
+	$accessResult = $objCheck -> check($userID, $accessToken);
+
+	if ($accessResult == 0) {
+		$sUserID = mysql_real_escape_string($_POST["sUserID"]);
+
+		if (is_numeric($sUserID)) {
+			/* データ をJSONで返す*/
+			include ("php/mysql2json.class.php");
+			//JSON整形してくれるクラスファイルinclude
+			//$result = mysql_query("select i.tagName,i.tagID,count(u.tagID) from tagInfo as i,userTag as u where categoryID = \"$categoryID\"");
+			$result = mysql_query("SELECT userID,userName,age,sex,receiveFlag,loginType,contents,thanks,points,setDate,loginTime FROM $table where userID = $sUserID LIMIT 1");
+			$num = mysql_affected_rows();
+			$objJSON = new mysql2json();
+			print(trim($objJSON -> getJSON($result, $num)));
+		} else {
+			echo 'false';
+		}
+	} else {
 		echo 'false';
 	}
+} else {
+	echo 'false';
 }
-else 
-{
-	echo 'false';		 
-}
-
-
 ?>
